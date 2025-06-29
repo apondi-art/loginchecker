17a215232adb562551fb831ff742b3b72eaf1e88
quinter <apondi-art>
2025-06-29 12:35:10 +0300

    feat:add fuction to get user by email

--- userauth/src/main/java/com/example/userauth/service/UserService.java
+++ userauth/src/main/java/com/example/userauth/service/UserService.java
@@ -6,26 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-// Marks this class as a service layer 
 @Service
 public class UserService {
-//Injects the UserRepository so we can talk to the database
+
     @Autowired
     private UserRepository userRepository;
 
-    //Injects a built-in Spring class for hashing passwords securely
     @Autowired
     private PasswordEncoder passwordEncoder;
-// Checks if a user with that email already exists 
+
+    // 🔹 Registers a new user
     public boolean registerUser(User user) {
         if (userRepository.findByEmail(user.getEmail()) != null) {
             return false; // email already used
         }
 
-        // encode (hash) the password before saving
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-        userRepository.save(user); // save user in DB
+        userRepository.save(user);
         return true;
+    }
+
+    // 🔹 Finds a user by email
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
